2294e31df85a0a9afeb2dfa61bdcdc02fccc86f6
Orim Dominic <orimdominic@gmail.com>
2021-02-21 22:19:42 +0100

    init

---
+++ landify/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      maxWidth: {
+        "8xl": "90rem",
+      },
+      minWidth: {
+        "1/2": "50%",
+        "1/3": "33.3%",
+        "1/4": "25%",
+        "1/5": "20%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
